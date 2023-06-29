6a3120d3d510076a10f749dc996096fe28b474ff
Cemund Malagar <cemund36@gmail.com>
2023-06-29 17:51:04 +0800

    improve ui and navigation

--- popup.js
+++ popup.js
@@ -11,13 +11,16 @@ document
       });
     })();
   });
-  // INFORMATION POP UP
-$(document).ready(function(){
-  $('#descPop').popover({
+
+// INFORMATION POP UP
+$(document).ready(function () {
+  $("#descPop").popover({
     placement: "left",
-    content: "A Chrome extension that helps users identify unfair or problematic clauses in website terms of service agreements. Click 'Detect' to start."
+    content:
+      "A Chrome extension that helps users identify unfair or problematic clauses in website terms of service agreements. Click 'Detect' to start.",
   });
 });
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.sentence) {
     fetch("http://127.0.0.1:5000/identifyUnfair", {
@@ -46,66 +49,92 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   // if all predictions is done
   if (request.count) {
     // handle request when done
-    console.log(request.count);
+    // console.log(request.count);
     // add number to navigate
-    const para = document.createElement("p");
-    para.setAttribute("id", "numberNavigate");
-    para.innerHTML =
-      "<span id='current-pos'>1</span> / <span id='total-pos'>" +
-      request.count +
-      "</span>";
-    document.body.appendChild(para);
+    // const para = document.createElement("p");
+    // para.setAttribute("id", "numberNavigate");
+    // para.innerHTML =
+    //   "<span id='current-pos'>1</span> / <span id='total-pos'>" +
+    //   request.count +
+    //   "</span>";
+    // document.body.appendChild(para);
 
-    // add button to navigate
-    const btn_nav = document.createElement("button");
-    btn_nav.setAttribute("id", "highlightNavigate");
-    btn_nav.innerText = "Navigate";
-    document.body.appendChild(btn_nav);
-    
-//     var detector = document.getElementById("scan");
-//           detector.setAttribute("hidden", "true");
-//           var navi = document.getElementById("navi");
-//           navi.removeAttribute("hidden");
+    // // add button to navigate
+    // const btn_nav = document.createElement("button");
+    // btn_nav.setAttribute("id", "highlightNavigate");
+    // btn_nav.innerText = "Navigate";
+    // document.body.appendChild(btn_nav);
 
-//           document.getElementById("close").addEventListener("click", function(){
-//             var navi = document.getElementById("navi");
-//             navi.setAttribute("hidden", "true");
-//             var detector = document.getElementById("scan");
-//             detector.removeAttribute("hidden");
-//           });
+    document.getElementById("total-pos").innerText = request.count;
 
-    document
-      .getElementById("highlightNavigate")
-      .addEventListener("click", function () {
-        var indexToNavigate = parseInt(
-          document.getElementById("current-pos").innerText
+    var detector = document.getElementById("scan");
+    detector.setAttribute("hidden", "true");
+    var navi = document.getElementById("navi");
+    navi.removeAttribute("hidden");
+
+    document.getElementById("close").addEventListener("click", function () {
+      var navi = document.getElementById("navi");
+      navi.setAttribute("hidden", "true");
+      var detector = document.getElementById("scan");
+      detector.removeAttribute("hidden");
+    });
+
+    // add eventlistener for right button
+    document.getElementById("right").addEventListener("click", function () {
+      var indexToNavigate = parseInt(
+        document.getElementById("current-pos").innerText
+      );
+
+      var totalPos = parseInt(document.getElementById("total-pos").innerText);
+
+      if (indexToNavigate == totalPos) {
+        indexToNavigate = 1;
+      } else {
+        indexToNavigate += 1;
+      }
+
+      // var idToNavigate = "highlight-" + indexToNavigate;
+      // console.log(idToNavigate);
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log(indexToNavigate);
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          { index_to_nav: indexToNavigate },
+          function (response) {}
         );
-
-        var totalPos = parseInt(document.getElementById("total-pos").innerText);
-
-        if (indexToNavigate == totalPos) {
-          indexToNavigate = 1;
-        } else {
-          indexToNavigate += 1;
-        }
-
-        // var idToNavigate = "highlight-" + indexToNavigate;
-        // console.log(idToNavigate);
-        chrome.tabs.query(
-          { active: true, currentWindow: true },
-          function (tabs) {
-            console.log(indexToNavigate);
-            chrome.tabs.sendMessage(
-              tabs[0].id,
-              { index_to_nav: indexToNavigate },
-              function (response) {}
-            );
-          }
-        );
-
-        document.getElementById("current-pos").innerText = indexToNavigate;
       });
+
+      document.getElementById("current-pos").innerText = indexToNavigate;
+    });
+
+    // add eventlistener for left button
+    document.getElementById("left").addEventListener("click", function () {
+      var indexToNavigate = parseInt(
+        document.getElementById("current-pos").innerText
+      );
+
+      var totalPos = parseInt(document.getElementById("total-pos").innerText);
+
+      if (indexToNavigate == 1) {
+        indexToNavigate = totalPos;
+      } else {
+        indexToNavigate -= 1;
+      }
+
+      // var idToNavigate = "highlight-" + indexToNavigate;
+      // console.log(idToNavigate);
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        console.log(indexToNavigate);
+        chrome.tabs.sendMessage(
+          tabs[0].id,
+          { index_to_nav: indexToNavigate },
+          function (response) {}
+        );
+      });
+
+      document.getElementById("current-pos").innerText = indexToNavigate;
+    });
+
     document.getElementById("highlightButton").remove();
   }
 });
-
